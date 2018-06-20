aba640a344bd165cde83ee1b46ec266288a7edbf
咚咚锵 <904885508@qq.com>
2018-06-20 23:11:00 +0800

    "写字板"

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-var div = document.getElementById('canvas')
-div.onmousedown = function(a){
-    var x = a.clientX
-    var y = a.clientY
-    var diva = document.createElement('div')
-    diva.style = "width:6px; height:6px;"+"background:green;border-radius:3px;"+
-    "position:absolute;left:"+(x-3)+"px;"+"top:"+(y-3)+"px;"
-    div.appendChild(diva)
-    console.log(a)
-}
+// var div = document.getElementById('canvas')
+// console.log(div)
+// div.onmousedown = function(a){
+//     var x = a.clientX
+//     var y = a.clientY
+//     console.log(div)    
+//     var diva = document.createElement('div')
+//     console.log(div)
+//     diva.style = "width:6px; height:6px;"+"background:green;border-radius:3px;"+
+//     "position:absolute;left:"+(x-3)+"px;"+"top:"+(y-3)+"px;"
+//     console.log(div)
+//     div.appendChild(diva)
+//     console.log(div)
+//     console.log(a)
+// }
